12db76914afe06fda15613ae96b8b50606dffb6e
Mathias Åhman <mathiasahman2@gmail.com>
2024-01-30 13:12:35 +0100

    User Model created

--- hcu_app/models.py
+++ hcu_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+# User Model
+class User(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    user_email = models.EmailField(max_length=200, unique=True)
+    user_password = models.CharField(max_length=50)
+    user_image = models.ImageField(default="/images/HCU_LOGO.jpg")
